3431d8a7162d9068b71ed8b06889b7ba47037000
Joeri Kassenaar <joeri.kassenaar@centagon.com>
2019-04-29 21:21:14 +0200

    draw the board

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="chessboard">
-    The Board
-  </div>
+	<div class="chessboard" >
+		<div class="row" v-for="file in [8,7,6,5,4,3,2,1]" :key="file" >
+			<div class="file-tile">{{ file }}</div>
+			<div v-for="(rankletter, ranknumber) in ['A','B','C','D','E','F','G','H']"
+				:key="`${rankletter}${file}`"
+				:class="tile(ranknumber, file)">
+				<!-- draw the piece -->
+			</div>
+			<div class="none-tile"></div>
+		</div>
+		<div class="row">
+			<div class="none-tile"></div>
+			<div v-for="rank in ['A','B','C','D','E','F','G','H']" :key="rank" class="rank-tile">
+				{{ rank }}
+			</div>
+			<div class="none-tile"></div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'ChessBoard',
+	name: 'ChessBoard',
+	methods: {
+		tile(rank, file) {
+			return ((file % 2) + (rank % 2)) % 2 === 0
+				? "light-tile"
+				: "dark-tile"
+			;
+		},
+	}
 }
 </script>
