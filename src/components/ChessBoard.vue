7d3e5f8bd3de5a92c6631dff155573fef34308d2
Joeri Kassenaar <joeri.kassenaar@centagon.com>
2019-04-29 21:48:21 +0200

    defines

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,17 +1,18 @@
 <template>
 	<div class="chessboard" >
-		<div class="row" v-for="file in [8,7,6,5,4,3,2,1]" :key="file" >
+		<div class="row" v-for="file in files" :key="file" >
 			<div class="file-tile">{{ file }}</div>
-			<div v-for="(rankletter, ranknumber) in ['A','B','C','D','E','F','G','H']"
-				:key="`${rankletter}${file}`"
-				:class="tile(ranknumber, file)">
+			<div v-for="(rankletter, ranknumber) in ranks"
+					:key="`${rankletter}${file}`"
+					:class="tile(ranknumber, file)">
 				<!-- draw the piece -->
 			</div>
 			<div class="none-tile"></div>
 		</div>
 		<div class="row">
 			<div class="none-tile"></div>
-			<div v-for="rank in ['A','B','C','D','E','F','G','H']" :key="rank" class="rank-tile">
+			<div v-for="rank in ranks"
+					:key="rank" class="rank-tile">
 				{{ rank }}
 			</div>
 			<div class="none-tile"></div>
@@ -22,6 +23,12 @@
 <script>
 export default {
 	name: 'ChessBoard',
+	data(){
+		return {
+			files: [ 8, 7, 6, 5, 4, 3, 2, 1],
+			ranks: ['A','B','C','D','E','F','G','H'],
+		}
+	},
 	methods: {
 		tile(rank, file) {
 			return ((file % 2) + (rank % 2)) % 2 === 0
